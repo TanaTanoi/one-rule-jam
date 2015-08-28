7b8f6a8f16de3f4912dd128161ac8083caa32a06
TanaTanoi <tana.tanoi@gmail.com>
2015-08-29 10:01:45 +1200

    -The Basic map class now works but only scales vertically.

--- src/maps/BasicMap.java
+++ src/maps/BasicMap.java
@@ -1,7 +1,8 @@
 package maps;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Rectangle;
 
@@ -11,44 +12,52 @@ import java.awt.Rectangle;
  *
  */
 public class BasicMap extends Map {
-	
+
 	Polygon bottom, top;
 	//array 1,2 top x,y 3,4 bottom,x,y
 	private static final int[][] polyPoints= {{0,0,10,10},{0,1,1,0},{0,0,10,10},{10,9,9,10}};
-	
+
 	public BasicMap(){
-		
+
 		top = new Polygon(polyPoints[0],polyPoints[1],4);
-		bottom = new Polygon(polyPoints[4],polyPoints[3],4);
-	}
-	
-	@Override
-	void draw(Graphics2D g, int canvasWidth, int canvasHeight) {
-		int boxSize = super.getDrawBoxSize(canvasWidth, canvasHeight);
-		int[][] localPolyPoints = polyPoints;
-		for(int[] array:localPolyPoints){
-			for(int i = 0;i<array.length;i++){
-				array[i] = array[i]*boxSize;
-			}
-		}
-		top = new Polygon(localPolyPoints[0],localPolyPoints[1],4);
-		bottom = new Polygon(localPolyPoints[4],localPolyPoints[3],4);
-		g.setColor(Color.red);
-		g.drawPolygon(top);
-		g.drawPolygon(bottom);
-		
+		bottom = new Polygon(polyPoints[2],polyPoints[3],4);
 	}
 
 	@Override
-	boolean contains(Rectangle rect) {
+	public void draw(Graphics g, int canvasWidth, int canvasHeight) {
+		int boxSize = super.getDrawBoxSize(canvasWidth, canvasHeight);
+		int[][] localPolyPoints = new int[4][4];
+		for(int j = 0; j < 4; j++){
+			for(int i = 0;i<4;i++){
+				localPolyPoints[j][i] = polyPoints[j][i]*boxSize;
+				System.out.println(localPolyPoints[j][i]);
+			}
+		}
+		top = new Polygon(localPolyPoints[0],localPolyPoints[1],4);
+		bottom = new Polygon(localPolyPoints[2],localPolyPoints[3],4);
+		g.setColor(Color.red);
+		g.fillPolygon(top);
+		g.fillPolygon(bottom);
+
+	}
+
+	@Override
+	public boolean intersects(Rectangle rect) {
 		return bottom.intersects(rect)||top.intersects(rect);
 	}
 
 	@Override
-	void translate(int deltaX, int deltaY) {
+	public boolean intersects(Point p) {
+		return bottom.contains(p)||top.contains(p);
+	}
+
+	@Override
+	public void translate(int deltaX, int deltaY) {
 		bottom.translate(deltaX, deltaY);
 		top.translate(deltaX, deltaY);
 
 	}
+
+
 
 }
--- src/maps/Map.java
+++ src/maps/Map.java
@@ -1,39 +1,54 @@
 package maps;
 
-import java.awt.Graphics2D;
+import java.awt.Graphics;
+import java.awt.Point;
 import java.awt.Rectangle;
 
 public abstract class Map {
 
 	private static final int BOX_SIZE = 10;
-	
-	
+
+
 	/**
-	 * Draws the Map onto the graphics pane using the canvas width and height 
-	 * as references to allow it to be resizeable and scalable. 
+	 * Draws the Map onto the graphics pane using the canvas width and height
+	 * as references to allow it to be resizeable and scalable.
 	 * @param g
 	 * @param canvasWidth
 	 * @param canvasHeight
 	 */
-	abstract void draw(Graphics2D g,int canvasWidth, int canvasHeight);
-	
+	abstract public void draw(Graphics g,int canvasWidth, int canvasHeight);
+
 	/**
-	 * Asks the map if the given polygon intercepts the bounding boxes
-	 * of the map.  
+	 * Asks the map if the given polygon intercepts the collision boxes
+	 * of the map.
 	 * @param poly
 	 * @return
 	 */
-	abstract boolean contains(Rectangle rect);
+	abstract public boolean intersects(Rectangle rect);
+
+	/**
+	 * Asks the map if the given point intercepts the
+	 * collision boxes of the map
+	 * @param p
+	 * @return
+	 */
+	abstract public boolean intersects(Point p);
+	/**
+	 * Asks the map if the player is currently achieving the selected rule.
+	 *
+	 * @param p
+	 * @return - true if they are obeying
+	 */
 	//abstract boolean assessRule(Player p);
-	
+
 	/**
 	 * Moves this map by the given delta x,y.
 	 * To be used for shifting the map to the left.
 	 * @param deltaX
 	 * @param deltaY
 	 */
-	abstract void translate(int deltaX, int deltaY);
-	
+	abstract public void translate(int deltaX, int deltaY);
+
 	int getDrawBoxSize(int canvasWidth, int canvasHeight){
 		return canvasHeight/BOX_SIZE;
 	}
